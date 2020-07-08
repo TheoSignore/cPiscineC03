2e63b2b3f4a5f5a96778cfbb3299fa3ee1fe3cc0
Theo SIGNORE <tsignore@e2r2p22.42.fr>
2020-07-08 17:49:09 +0200

    ex00 reviewed and modified

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 18:57:15 by tsignore          #+#    #+#             */
-/*   Updated: 2020/07/07 21:00:09 by tsignore         ###   ########.fr       */
+/*   Updated: 2020/07/08 17:48:35 by tsignore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,13 @@ int	ft_strlen(char *str)
 int	ft_strcmp(char *s1, char *s2)
 {
 	int i;
+	int s1_len;
+	int s2_len;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	while (i <= s1_len && i <= s2_len)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
